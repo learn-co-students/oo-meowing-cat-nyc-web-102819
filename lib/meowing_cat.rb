fe3d96231e2378d50ed690a025389296b6d48269
Lilibeth Santo <itzlilibeth@gmail.com>
2019-10-30 04:01:09 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -3,12 +3,9 @@ class Cat
   attr_reader :name
   attr_writer :name 
   attr_accessor :name
+  
+    def meow
+    puts "meow!"
+  end
 end
 
-maru = Cat.new
-maru.name = "Maru"
- 
-maru.name
-
-maru.meow
- 
